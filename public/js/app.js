4c6a15a9ea01a609af7c6263719f68b23dc1a1a8
vagrant <vagrant@homestead>
2016-01-27 16:56:20 +0000

    gitignore working, has bg now

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Eric on 1/26/2016.
+ */
+$(document).ready(function(){
+    $(window).scroll(function() {
+        var x = $(this).scrollTop();
+        $('#main').css('background-position', '100% ' + parseInt(-x / 1) + 'px' + ', 0% ' + parseInt(-x / 2) + 'px, center top');
+    });
+});
