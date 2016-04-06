39a896a04c503e4e88dc2390ae874369c981f034
Eric Penner <epenner@unomaha.edu>
2016-04-06 22:04:56 +0000

    Updated gitignore, changed some Jquery

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,6 @@
 $(document).ready(function(){
     $(window).scroll(function() {
         var x = $(this).scrollTop();
-        $('#main').css('background-position', '100% ' + parseInt(-x / 1) + 'px' + ', 0% ' + parseInt(-x / 2) + 'px, center top');
+        $('#main').css('background-position', '100% ' + parseInt(-x / 4) + 'px' + ', 0% ' + parseInt(-x / 4) + 'px, center top');
     });
 });
